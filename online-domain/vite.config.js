88655847351430e1eda7ae25b202b0bfd3861e06
O-Cube <oyandut2013@gmail.com>
2025-08-27 10:08:46 +0200

    extension changed to .js

--- online-domain/vite.config.js
+++ online-domain/vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: 'http://www.obed-oyandut.com/',
+  base: 'http://www.obed-oyandut.com',
 })
